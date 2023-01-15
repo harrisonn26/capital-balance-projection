576428701d9c3d2910efe7158ebaee43edf7cbb7
Harrison Norris <harrisonnorris26@gmail.com>
2023-01-15 19:22:00 +1100

    Removed boilerplate code and setup ProjectionData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+type ProjectionData = {
+  startBalance: number;
+  withdrawalAge: number;
+  salary: number;
+  contributionRate: number;
+  inflationRate: number;
+  earnings: number;
+  fees: number;
+  tax: number;
+  withdrawalRate: number;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CapitalBalanceProjection';
+  projectionData: ProjectionData = {
+    startBalance: 0,
+    withdrawalAge: 0,
+    salary: 0,
+    contributionRate: 9.5,
+    inflationRate: 3,
+    earnings: 7.5,
+    fees: 2,
+    tax: 15,
+    withdrawalRate: 10,
+  };
 }
